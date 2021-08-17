fb7017e36ad397388dd3a691db9cd72a37acb9a0
saert3311 <saert@outlook.com>
2021-08-17 19:27:42 -0400

    Dia 6 Semana 8

---
+++ modulo_06/02-control_flujo.rb
@@ -0,0 +1,7 @@
+#se capta los datos por consola a una variable
+print 'Ingrese Nombre: '
+nombre = gets.chomp
+print 'Ingrese Apellido: '
+apellido = gets.chomp
+# se imprime por pantalla
+puts "Hola a todos, me llamo #{nombre} #{apellido}!!!"
